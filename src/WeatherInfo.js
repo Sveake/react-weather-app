0924a286f832588438bddd850a287c68bca18dd4
Sveake <51757935+Sveake@users.noreply.github.com>
2021-05-31 20:16:41 +0300

    Added Search Engine

---
+++ src/WeatherInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+
+export default function WeatherInfo (props) {
+    return (
+        <div className="weatherInfo">
+<h2 className="city">{props.data.city}</h2>
+<ul className="dateTime">
+  <li><FormatDate date={props.data.date} /></li> 
+  <li>Updated at 16:00</li>
+</ul>
+<hr />
+<div>
+    <ul className="temperature">
+      <li>{Math.round(props.data.temperature)} <span className="celsius">C</span> | <span className="fahrenheit">F</span></li>
+      <li className="text-capitalize">{props.data.description}</li>
+    </ul>
+<div className="row">
+      <div className="col-2">
+        <img src={props.data.icon} alt="weather" width="80" />
+      </div>
+  <div className="col-4">
+    <ul className="weather-data">
+      <li>Humidity: {props.data.humidity} %</li>
+      <li>
+      Wind speed: {props.data.wind} km / h
+      </li>
+    </ul>
+  </div>
+  </div>
+</div>
+</div>
+    );
+}
--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./weather.css";
 import axios from "axios";
-import FormatDate from "./FormatDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather () {
 const [weatherData, setWeatherData] = useState({});
 const [ready, setReady] = useState(false);
+const [city, setCity] = useState({});
 
 
 
@@ -18,50 +19,39 @@ function handleResponse(response) {
     city: response.data.name,
     description: response.data.weather[0].description,
     date: new Date(response.data.dt * 1000),
+    icon: "http://openweathermap.org/img/wn/10d@2x.png",
   });
   setReady(true);
+}
+
+function search() {
+  const apiKey = "9aef592de78a13851ffe5a565ea13c5f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
+
+function handleSubmit(event) {
+  event.preventDefault()
+  search();
+}
+
+function handleCity(event) {
+setCity(event.target.value);
 }
 
 if (ready) {
   return (
     <div className="weather">
-<h2 className="city">{weatherData.city}</h2>
-<form className="Search">
-  <input type="search" placeholder="Write city here" autoFocus="on" />
+<form onSubmit={handleSubmit}>
+  <input type="search" placeholder="Write city here" autoFocus="on" onChange={handleCity}/>
   <input type="submit" value="Submit" className="btn btn-info" />
 </form>
-<ul className="dateTime">
-  <li><FormatDate date={weatherData.date} /></li> 
-  <li>Updated at 16:00</li>
-</ul>
-<hr />
-<div>
-    <ul className="temperature">
-      <li>{Math.round(weatherData.temperature)} <span className="celsius">C</span> | <span className="fahrenheit">F</span></li>
-      <li className="text-capitalize">{weatherData.description}</li>
-    </ul>
-<div className="row">
-      <div className="col-2">
-        <img src="https://cdn3.iconfinder.com/data/icons/weather-610/64/weather_drizzle_rain_cloud-512.png" alt="weather" width="80" />
-      </div>
-  <div className="col-4">
-    <ul className="weather-data">
-      <li>Humidity: {weatherData.humidity} %</li>
-      <li>
-      Wind speed: {weatherData.wind} km / h
-      </li>
-    </ul>
-  </div>
-  </div>
-</div>
+<WeatherInfo data={weatherData} />
+
     </div>
   );
 } else {
-  const apiKey = "9aef592de78a13851ffe5a565ea13c5f";
-  let city = "Tallinn";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
+search();
   return "Loading";
 }
 
