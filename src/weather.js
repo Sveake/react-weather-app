31df74c239ad80a96c0d0060bb152b965b82bc72
Sveake <51757935+Sveake@users.noreply.github.com>
2021-05-31 19:19:16 +0300

    Added API to weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
+import axios from "axios";
 
 export default function Weather () {
+const [weatherData, setWeatherData] = useState({});
+const [ready, setReady] = useState(false);
+
+
+
+function handleResponse(response) {
+  console.log(response.data);
+  setWeatherData({
+    temperature: response.data.main.temp,
+    wind: response.data.wind.speed,
+    humidity: response.data.main.humidity,
+    city: response.data.name,
+    description: response.data.weather[0].description,
+    date: "Monday May 31st, 2021"
+  });
+  setReady(true);
+}
+
+if (ready) {
   return (
     <div className="weather">
-<h2 className="city">Tallinn</h2>
+<h2 className="city">{weatherData.city}</h2>
 <form className="Search">
   <input type="search" placeholder="Write city here" autoFocus="on" />
   <input type="submit" value="Submit" className="btn btn-info" />
 </form>
 <ul className="dateTime">
-  <li>May 31, 2021</li>
+  <li>{weatherData.date}</li> 
   <li>Updated at 16:00</li>
 </ul>
 <hr />
 <div>
     <ul className="temperature">
-      <li>Temperature: 20 <span className="celsius">C</span> | <span className="fahrenheit">F</span></li>
-      <li>Cloudy</li>
+      <li>{Math.round(weatherData.temperature)} <span className="celsius">C</span> | <span className="fahrenheit">F</span></li>
+      <li>{weatherData.description}</li>
     </ul>
 <div className="row">
       <div className="col-2">
@@ -25,11 +45,9 @@ export default function Weather () {
       </div>
   <div className="col-4">
     <ul className="weather-data">
-      <li>Humidity: 80%</li>
-      <li>Precipitation: 15%
-      </li>
+      <li>Humidity: {weatherData.humidity} %</li>
       <li>
-      Wind speed: 5 km / h
+      Wind speed: {weatherData.wind} km / h
       </li>
     </ul>
   </div>
@@ -37,4 +55,14 @@ export default function Weather () {
 </div>
     </div>
   );
+} else {
+  const apiKey = "9aef592de78a13851ffe5a565ea13c5f";
+  let city = "Tallinn";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "Loading";
+}
+
+  
 }
